c9e3feff9f47f293e1d43c26862e834a446ac370
Oskar Börjesson <o.borjesson@gmail.com>
2012-05-06 13:22:00 +0200

    Cosmetic changes and rudimentary UI for relations

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ App.Node = Em.Object.extend({
     this.attributes.pushObject(App.Attribute.create())
     }
 });
+App.Relation = Em.Object.extend({
+  init: function(){
+      this._super();
+      this.attributes=[];
+      this.type= "is_related_to";
+      this.from="0";
+      this.to="1";
+  }
+})
 App.Attribute = Em.Object.extend({
   init: function(key,value){
       this._super();
@@ -27,21 +36,50 @@ App.nodeController = Em.ArrayController.create({
 
   // Creates a new todo with the passed title, then adds it to the array.
   createNode: function() {
+      if(this.content.length>=4){
+          alert("For layout reasons you cannot add more than 4 nodes currently :)")
+      }
+      else{
     var node = App.Node.create();
     this.pushObject(node);
+      }
+  }
+});
+App.relationController = Em.ArrayController.create({
+
+  // Initialize the array controller with an empty array.
+  content: [],
+
+  // Creates a new todo with the passed title, then adds it to the array.
+  createRelation: function() {
+      if(this.content.length>=4){
+          alert("For layout reasons you cannot add more than 4 relations currently :)")
+      }
+      else{
+    var rel = App.Relation.create();
+    this.pushObject(rel);
+      }
   }
 });
 
 App.AddNode = Ember.Button.extend({
-    classNames: ['add-node-view','btn','btn-success'],
+    classNames: ['add-node-view','btn','btn-success','btn-small'],
             target: 'App.nodeController',
             action: 'createNode'
     });
+    App.AddRelation = Ember.Button.extend({
+    classNames: ['add-rel-view','btn','btn-success','btn-small'],
+            target: 'App.relationController',
+            action: 'createRelation'
+    });
 
-App.AddAttribute = Ember.Button.extend({
-    classNames: ['add-attr-view','btn','btn-success','btn-small'],
-            target: 'contentView',
-            action: 'addAttribute'
+App.AddAttribute = Ember.View.extend({
+    tagName:'div',
+    classNames: ['add-attr-view','btn'],
+    click: function(){
+        var cv=this.get('contentView');
+        cv.addAttribute();
+    }
     });
     
 App.NodeView = Em.View.extend({
@@ -57,13 +95,13 @@ App.AttributeView = Em.View.extend({
   templateName: 'attribute-template'
 })
  App.Text = Ember.TextField.extend({
-     classNames: ['input-small','inline']
+     classNames: ['attribute','form-inline']
  }
  );
  
  App.DeleteAttributeView = Ember.View.extend({
      tagName:'i',
-  classNames: ['delete-attr-view','icon-minus-sign'],
+  classNames: ['delete-attr-view','icon-remove'],
   click: function() {
     var attrView = this.get('content');
     var node = attrView.getPath('parentView.content');
@@ -73,9 +111,16 @@ App.AttributeView = Em.View.extend({
   }
 });
 
-config= {};
-App.load = function(){
+App.RelationView=App.NodeView.extend({
+  classNames: ['relation-view'],
+  templateName: 'relation-template'
+})
+
+
+App.getConfig = function(){
+  var config={};
     config.nodes=[];
+    config.relations=[];
     config.parseNode= function(node) { 
     var tempObject ={};
     node.get('attributes').forEach(function(attr){
@@ -83,7 +128,19 @@ App.load = function(){
     })
     config.nodes.pushObject(tempObject);
     }
+        config.parseRelation= function(relation) { 
+    var tempObject ={};
+    relation.get('attributes').forEach(function(attr){
+        tempObject[attr.get('key')]=attr.get('value')
+    })
+    tempObject.type=relation.get('type');
+    tempObject.to=relation.get('to');
+    tempObject.from=relation.get('from');
+    config.relations.pushObject(tempObject);
+    }
     App.nodeController.get('content').forEach(config.parseNode);
+    App.relationController.get('content').forEach(config.parseRelation);
+    return config;
 }
 
 
